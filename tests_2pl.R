d27295282d9ebf782276fc79824fe11257787760
Krzysztof Jędrzejewski <krzysztof.jedrzejewski@pearson.com>
2018-07-01 00:54:00 +0000

    Code examples

--- tests_2pl.R
+++ tests_2pl.R
@@ -298,9 +298,6 @@ save(results_greta, file = 'data/results_greta.RData')
 items = seq(50, 500, 50)
 persons = seq(500, 5000, 500)
 
-items = seq(550, 1000, 50)
-persons = seq(5500, 10000, 500)
-
 results_greta_gpu = tibble()
 
 for(i in 1:length(items)) {
